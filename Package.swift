16a429dd1d43b2493d7af94fc96ab375d17c9065
Andreas Lang <andreas.lang@andrena.de>
2019-12-18 12:29:51 +0100

    Removed CLI from package

--- Package.swift
+++ Package.swift
@@ -8,43 +8,13 @@ import PackageDescription
 let package = Package(
    name: "swiftymocky",
    products: [
-       .executable(name: "swiftymocky", targets: ["SwiftyMockyCLI"]),
        .library(name: "SwiftyMocky", targets: ["SwiftyMocky"]),
-   ],
-   dependencies: [
-       .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-       .package(url: "https://github.com/tuist/xcodeproj.git", from: "7.1.0"),
-       .package(url: "https://github.com/jianstm/Crayon", from: "0.0.3"),
-       .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
-       .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-       .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
    ],
    targets: [
        .target(
            name: "SwiftyMocky",
            path: "./Sources/Classes"
            ),
-       .target(
-           name: "SwiftyMockyCLI",
-           dependencies: [
-               "Commander",
-               "SwiftyMockyCLICore"
-           ]),
-       .target(
-           name: "SwiftyMockyCLICore",
-           dependencies: [
-               "ShellOut",
-               "Crayon",
-               "XcodeProj",
-               "PathKit",
-               "Yams",
-           ]),
-       .testTarget(
-           name: "SwiftyMockyCLICoreTests",
-           dependencies: [
-               "SwiftyMockyCLICore",
-               "SwiftyMocky"
-           ]),
    ]
 )
 #else
@@ -52,44 +22,12 @@ let package = Package(
 let package = Package(
     name: "swiftymocky",
     products: [
-        .executable(name: "swiftymocky", targets: ["SwiftyMockyCLI"]),
         .library(name: "SwiftyMocky", targets: ["SwiftyMocky"]),
-    ],
-    dependencies: [
-       .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-       .package(url: "https://github.com/jianstm/Crayon", from: "0.0.3"),
-       .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
-       .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-       .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
     ],
     targets: [
         .target(
             name: "SwiftyMocky",
             path: "./Sources/Classes"),
-        .target(
-            name: "SwiftyMockyCLI",
-            dependencies: [
-                "Commander",
-                "SwiftyMockyCLICore"
-            ]),
-        .target(
-            name: "SwiftyMockyCLICore",
-            dependencies: [
-                "ShellOut",
-                "Crayon",
-                "PathKit",
-                "Yams",
-            ],
-            exclude: [
-                "Commands/MacOS",
-                "Utils/MacOS",
-            ]),
-        .testTarget(
-            name: "SwiftyMockyCLICoreTests",
-            dependencies: [
-                "SwiftyMockyCLICore",
-                "SwiftyMocky"
-            ]),
     ]
 )
 #endif
